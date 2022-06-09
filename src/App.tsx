9c69467691b9c823133bef29d220393e537d4770
sara_carolina12 <saracarogode@gmail.com>
2022-06-08 19:37:13 -0500

    'logo-buttons'

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import launcherImage from './assets/launcher-imagen.png'
 import windowBackground from './assets/launcher-ventana.png'
 import bitmonLogo from './assets/logo bitmon.png'
+import minimizar from './assets/launcher-minimizar.png'
+import equis from './assets/launcher-cerrar.png'
 
 import './App.css'
 import { DownloadManager } from './components/DownloadManager/DownloadManager'
@@ -10,6 +12,7 @@ function App() {
     return (
         <div className="App" >
             <div>
+                
                 <img
                     className="Bitmon-background"
                     src={windowBackground}
@@ -25,6 +28,16 @@ function App() {
                     src={bitmonLogo}
                     alt='bitmon logo'
                 ></img>
+                <button 
+                    className="x-btn"
+                    // onClick={exitGame}
+                >
+                </button>
+                <button 
+                    className="min-btn"
+                    // onClick={exitGame}
+                > 
+                </button>
                 <h1 className='game-title'>Bitmon Alpha Launcher</h1>
                 <DownloadManager></DownloadManager>
             </div>
